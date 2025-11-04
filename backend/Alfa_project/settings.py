293c2c12f44a4fd82c94e35edcbf5230c60777e4
Douglas Farias Rodrigues <douglasfariasrodrigues@gmail.com>
2025-11-04 00:10:32 -0300

    Melhorias de segurança: variáveis de ambiente, CORS e validação de senhas

--- backend/Alfa_project/settings.py
+++ backend/Alfa_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Construa caminhos dentro do projeto assim: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Veja https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # AVISO DE SEGURANÇA: mantenha a chave secreta usada em produção secreta!
-SECRET_KEY = 'django-insecure-3tz@=p$fc=9^@+mi)atwc%+llvkon2(#(&$1frhy)j*+z(aria'
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-3tz@=p$fc=9^@+mi)atwc%+llvkon2(#(&$1frhy)j*+z(aria')
 
 # AVISO DE SEGURANÇA: não execute com debug ativado em produção!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
 
 
 # Definição da aplicação
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'rest_framework_simplejwt',
     'drf_spectacular',
@@ -45,6 +47,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,6 +129,10 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CORS settings
+CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', default='http://localhost:3000,http://127.0.0.1:3000').split(',')
+CORS_ALLOW_CREDENTIALS = True
 
 # Configurações do Django REST Framework
 REST_FRAMEWORK = {
